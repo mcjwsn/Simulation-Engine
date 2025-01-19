2c2e422d593ca5d29ca5f03f177faa3999dba1a0
mcjwsn <maciek.wisniewski04@gmail.com>
2025-01-19 14:43:23 +0100

    old comments deleted

--- oolab/src/main/java/agh/ics/oop/model/SimulationManager.java
+++ oolab/src/main/java/agh/ics/oop/model/SimulationManager.java
@@ -3,7 +3,6 @@ package agh.ics.oop.model;
 import agh.ics.oop.Simulation;
 import agh.ics.oop.Statistics;
 import agh.ics.oop.model.Enums.MapType;
-import agh.ics.oop.model.util.ConvertUtils;
 
 import java.util.*;
 
@@ -97,15 +96,12 @@ public class SimulationManager {
 
     private void deleteDeadAnimals() {
         Set<Animal> animalsToRemove = new HashSet<>(simulation.getAnimals());
-       // System.out.println("Checking " + animalsToRemove.size() + " animals for death");
 
         for(Animal animal : animalsToRemove) {
             if (animal.getEnergy() <= 0) {
-              //  System.out.println("Animal at " + animal.getPosition() + " died with energy: " + animal.getEnergy());
                 animal.setDeathDate(simulationProperties.getDaysElapsed());
                 map.removeAnimal(animal);
                 simulation.getAnimals().remove(animal);
-               // System.out.println("Remaining animals: " + simulation.getAnimals().size());
             }
         }
     }
@@ -131,65 +127,30 @@ public class SimulationManager {
 
     private void owlBearEat()
     {
-//        Set<Animal> animalsToRemove = new HashSet<>(((OwlBearMap)map).eatAnimals());
-//        System.out.println("Checking " + animalsToRemove.size() + " animals for death");
-//
-//        for(Animal animal : animalsToRemove) {
-//            System.out.println("Animal at " + animal.getPosition() + " eaten by OwlBear with energy: " + animal.getEnergy());
-//            animal.setDeathDate(simulationProperties.getDaysElapsed());
-//            simulation.getAnimals().remove(animal);
-//            System.out.println("Remaining animals: " + simulation.getAnimals().size());
-//        }
         Set<Animal> animalsToRemove = new HashSet<>(simulation.getAnimals());
-       // System.out.println("Checking " + animalsToRemove.size() + " animals for being eaten");
 
         for(Animal animal : animalsToRemove) {
             if (animal.getPosition().equals(((OwlBearMap)map).getOwlBearPosition()))
             {
-              //  System.out.println("Animal at " + animal.getPosition() + " eaten by OwlBear with energy: " + animal.getEnergy());
                 animal.setDeathDate(simulationProperties.getDaysElapsed());
                 simulation.getAnimals().remove(animal);
                 map.removeAnimal(animal);
-              //  System.out.println("Remaining animals: " + simulation.getAnimals().size());
             }
         }
     }
 
 
     public void reproduceAnimals() {
-       // System.out.println("\n=== Starting reproduction phase ===");
-       // System.out.println("Total positions with animals: " + map.getAnimals().keySet().size());
-      //  System.out.println(map.getAnimals().keySet());
-
         for (Vector2d position : map.getAnimals().keySet()) {
             List<Animal> animalList = map.getAnimals().get(position);
-            //System.out.println("\nChecking position " + position + " with " + animalList.size() + " animals");
             if (animalList.size() > 1) {
                 Animal a1 = animalList.get(0);
                 Animal a2 = animalList.get(1);
-               // System.out.println("Potential parents found:");
-                //System.out.println("Parent 1 energy: " + a1.getEnergy());
-               // System.out.println("Parent 2 energy: " + a2.getEnergy());
-               // System.out.println("Energy needed to reproduce: " + simulationProperties.getEnergyLevelNeededToReproduce());
-               // System.out.println(position);
                 if (a1.getEnergy() > simulationProperties.getEnergyLevelNeededToReproduce() &&
                         a2.getEnergy() > simulationProperties.getEnergyLevelNeededToReproduce()) {
-
-                    //System.out.println("Both parents have enough energy - creating child");
                     int[] getGenome = Genes.mixGenesFromParents(a1,a2,simulationProperties);
                     Animal child = new Animal(position, simulationProperties,getGenome);
-                   // System.out.println("First partent genome");
-                    //System.out.println(Arrays.toString(a1.getGenome()));
-                   // System.out.println("Second parent genowe");
-                    //System.out.println(Arrays.toString(a2.getGenome()));
-                   // System.out.println("child genome");
-                   // System.out.print(Arrays.toString(child.getGenome()));
                     synchronized (this) {
-                       // System.out.println("Before reproduction:");
-                        //System.out.println("Total animals at position before birth: " +
-                              //  map.getAnimals().get(position).size());
-                       // System.out.println("Parent 1 energy: " + a1.getEnergy());
-                      //  System.out.println("Parent 2 energy: " + a2.getEnergy());
 
                         map.getAnimals().get(position).add(child);
                         simulation.addAnimal(child);
@@ -200,24 +161,12 @@ public class SimulationManager {
                         a2.removeEnergy(simulationProperties.getEnergyLevelToPassToChild());
                         a2.addChildToList(simulation.getAnimals().get(simulation.getAnimals().indexOf(child)));
                         a2.increaseChildrenNumber();
-
-                       // System.out.println("After reproduction:");
-                      //  System.out.println("Parent 1 energy: " + a1.getEnergy());
-                       // System.out.println("Parent 2 energy: " + a2.getEnergy());
-                       // System.out.println("Child energy: " + child.getEnergy());
-                      //  System.out.println("Total animals at position after birth: " +
-                           //     map.getAnimals().get(position).size());
-                       // System.out.println("\n");
                     }
                 } else {
-                   // System.out.println("Not enough energy for reproduction");
                 }
             } else {
-               // System.out.println("Not enough animals for reproduction at this position");
             }
         }
-        //System.out.println("\n=== Reproduction phase completed ===");
-        //System.out.println("Total animals after reproduction: " + simulation.getAnimals().size());
     }
 
     public void eat() {
@@ -227,7 +176,6 @@ public class SimulationManager {
                 List<Animal> animalList = map.getAnimals().get(position);
                 if (!animalList.isEmpty()) {
                     Animal animal = animalList.get(0);
-                    //System.out.println("Animal at " + position + " eating grass. Current energy: " + animal.getEnergy());
                     synchronized (this) {
                         animal.eat(simulationProperties.getGrassEnergy());
                         Grass eatenGrass = map.getPlants().get(position);
@@ -235,35 +183,10 @@ public class SimulationManager {
                         map.getFreePositionsForPlants().add(position);
                         restoreEatenPlantPosition(eatenGrass);
                     }
-                    //System.out.println("After eating energy: " + animal.getEnergy());
                 }
             }
         }
     }
-
-//    public void initializePositions(AbstractWorldMap map) {
-//        int equatorHeight = simulationProperties.getEquatorHeight();
-//        int width = map.getWidth();
-//        int height = map.getHeight();
-//        Set<Vector2d> preferred = new HashSet<>();
-//        Set<Vector2d> lessPreferred = new HashSet<>();
-//        int startEquatorRow = (height - 1) / 2;
-//        int endEquatorRow = height / 2;
-//        for (int x = 0; x <= height; x++) {
-//            for (int y = 0; y <= width; y++) {
-//                Vector2d position = new Vector2d(x, y);
-//                if (y >= startEquatorRow + 1 && y <= endEquatorRow + 1) {
-//                    preferred.add(position);
-//                } else {
-//                    lessPreferred.add(position);
-//                }
-//            }
-//        }
-//        preferredPositions.clear();
-//        preferredPositions.addAll(preferred);
-//        lessPreferredPositions.clear();
-//        lessPreferredPositions.addAll(lessPreferred);
-//    }
 public void initializePositions(AbstractWorldMap map) {
     int equatorHeight = simulationProperties.getEquatorHeight(); // The height of the equator
     int width = map.getWidth();
@@ -320,5 +243,4 @@ public void initializePositions(AbstractWorldMap map) {
     public void growGrass() {
         generateGrass(DAILY_GRASS_NUMBER);
     }
-
 }
